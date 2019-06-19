655ef80349913ae11c0a8413b37b6cd2eb11b611
Alexandre Rieux <alexr.3165@gmail.com>
2019-06-19 16:47:42 +0100

    Update lambda

--- lambda.js
+++ lambda.js
@@ -1,9 +1,23 @@
 const { pullRequestHook } = require('./handlers');
+const qs = require('qs');
 
 module.exports.pullRequest = async event => {
-  const payload = JSON.parse(event.body);
+  if (typeof event.body !== 'string') {
+    console.error(`Body is not a string ${typeof event.body}`);
+  }
 
-  const response = await pullRequestHook(payload);
+  const requestBody = qs.parse(event.body);
+
+  if (!requestBody.payload) {
+    console.error('Payload is undefined, body: ' + event.body);
+
+    return Promise.resolve({
+      statusCode: 500,
+      body: 'Payload not parsed'
+    });
+  }
+
+  const response = await pullRequestHook(requestBody.payload);
 
   return Promise.resolve({
     statusCode: response.status,
